6c4128d4e479dae379b4e22ebff40b53df02b11f
acimovicAlek <acimovic.alek@gmail.com>
2018-05-12 20:28:25 +0200

    Add fe

--- PostModule/src/main/java/com/meminator/postmodule/Configurations/AsyncConfig.java
+++ PostModule/src/main/java/com/meminator/postmodule/Configurations/AsyncConfig.java
@@ -23,7 +23,7 @@ import javax.management.Query;
 public class AsyncConfig implements RabbitListenerConfigurer {
 
     public static final String EXCHANGE_USERS_NAME = "user-queue-exchange";
-    public static final String ROUTING_KEY_USERS = "*user.create";
+    public static final String ROUTING_KEY_USERS = "user.create";
     public static final String QUEUE_NAME_USERS = "usersQueue";
     public static final String EXCHANGE_VOTE_NAME = "vote-queue-exchange";
     public static final String ROUTING_KEY_VOTE = "*.vote";
