4e1496e163b23c2621ab30315926f885884ed119
acimovicAlek <acimovic.alek@gmail.com>
2018-06-04 16:47:36 +0200

    Fix it like you mean it

--- UserModule/src/main/java/com/meminator/userModule/controllers/RegisteredUserController.java
+++ UserModule/src/main/java/com/meminator/userModule/controllers/RegisteredUserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +23,6 @@ import forms.RegisteredUser.ResetPasswordForm;
 import forms.RegisteredUser.UpdateInfoForm;
 import io.swagger.annotations.Api;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequestMapping(value="/users")
 @Api(value="registered user")
@@ -32,7 +30,6 @@ public class RegisteredUserController {
 	@Autowired
 	RegisteredUserService registeredUserService;
 	
-	@PreAuthorize("isAnonymous() or hasRole('ROLE_user')")
 	@RequestMapping(value = "/{username}", method=RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity getUserByUsername(@PathVariable final String username) {
@@ -43,7 +40,6 @@ public class RegisteredUserController {
 		}
 	}
 	
-	@PreAuthorize("isAnonymous() or hasRole('ROLE_user')")
 	@RequestMapping(value = "/search/{username}", method=RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity searchUserByUsername(@PathVariable final String username) {
@@ -54,8 +50,7 @@ public class RegisteredUserController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error ocurred.");
 		}
 	}
-	
-	@PreAuthorize("isAnonymous()")
+
 	@RequestMapping(value = "/register", method=RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<Boolean> createUser(@RequestBody @Valid final CreateUserForm createUserForm) {
