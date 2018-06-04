5358c011d5d7c49583f08aff74e0a22f2c448224
acimovicAlek <acimovic.alek@gmail.com>
2018-06-04 18:11:42 +0200

    Front in progress

--- meminator_fe/src/components/header/Header.js
+++ meminator_fe/src/components/header/Header.js
@@ -13,16 +13,9 @@ class Header extends Component{
                 {sessionStorage.getItem("username") && <Notification />}
                 <NavItem href='get-started.html'><Icon>home</Icon></NavItem>
                 <NavItem href='get-started.html'><Icon>explore</Icon></NavItem>
-<<<<<<< HEAD
                 {sessionStorage.getItem("token")===null?
                     <NavItem href='/login'><Icon>lock</Icon></NavItem>
                     : <NavItem onClick={() => {sessionStorage.clear(); window.location = "/"}}><Icon>input</Icon></NavItem>}
-=======
-                <NavItem href='get-started.html'><Icon>input</Icon></NavItem>
-                {sessionStorage.getItem("username") &&
-                <Notification />
-            }
->>>>>>> e2c38fa81b5df69248a312d47d88fcbd3a5ae189
             </Navbar>
         );
     }
